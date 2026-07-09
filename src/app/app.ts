fb2d9e0a2f3ae81527f20070e459048ddf507e0e
DIOGO MENEZES CALAZANS <diogo.calazans@aluno.senac.rio>
2026-07-08 21:35:14 -0300

    primeiro envio de arquivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EpiList } from './epi-list/epi-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EpiList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('seguranca-frontend');
+ titulo = "Sistema de controle de EPIs";
+ subtitulo = "Gestão  de seguraça do trabalho";
+ descricao = "Aplição para contole, monitoramento e conformidade dos equipamentos de proteção individual";
+ 
 }
